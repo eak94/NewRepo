af72d3b105707e7e389bf801662c917efd71c370
eak94 <bykonyasha@main.ru>
2024-02-27 18:46:25 +0700

    Добавлено в классе Person имя, фамилия, возраст

--- ConsoleApp1/ClassLibrary1/Person.cs
+++ ConsoleApp1/ClassLibrary1/Person.cs
@@ -2,6 +2,20 @@
 {
     public class Person
     {
+        /// <summary>
+        /// Имя пользователя
+        /// </summary>
+        private string _name;
+
+        /// <summary>
+        /// Фамилия пользователя
+        /// </summary>
+        private string _surname;
+
+        /// <summary>
+        /// Возраст пользователя 
+        /// </summary>
+        private int _age;
 
     }
 }
