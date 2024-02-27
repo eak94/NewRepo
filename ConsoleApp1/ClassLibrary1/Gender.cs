323629b6e9e7d075d82c03748b94d5c327821861
eak94 <bykonyasha@main.ru>
2024-02-27 18:42:44 +0700

    Создание класса gender

---
+++ ConsoleApp1/ClassLibrary1/Gender.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    /// <summary>
+    /// Пол
+    /// </summary>
+    internal class Gender
+    {
+        Male,
+        Female,
+        Undefined
+    }
+}
