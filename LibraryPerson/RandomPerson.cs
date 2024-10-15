bf4092ec21d3348b8ee7a9f560c848fbf9ab9435
eak94 <bykonyasha@main.ru>
2024-10-15 12:48:44 +0700

    fix LB2 Random

--- LibraryPerson/RandomPerson.cs
+++ LibraryPerson/RandomPerson.cs
@@ -1,4 +1,6 @@
 
+using System.Threading;
+
 namespace LibraryPerson
 {
     /// <summary>
@@ -7,31 +9,46 @@ namespace LibraryPerson
     public class RandomPerson
     {
         /// <summary>
-        /// Метод для генерации случайных взрослых
+        /// Метод для генерации случайного мужчины
         /// </summary>
-        /// <returns>Возвращает персону, которая сгенерирована случайным образом</returns>
-        public static void GetRandomPerson(PersonBase person)
+        /// <returns>Возвращает мужчину, которая сгенерирована случайным образом</returns>
+        public static void MaleRandomPerson(PersonBase person)
         {
-            PersonBase randomPerson = new Random();
-
-            List<string> nameFemale = new List<string>()
-            {
-                "Валерия", "Алена", "Анна",
-                "Екатерина", "Алина", "Кира",
-                "Валентина"
-            };
+            Random randomMalePerson = new Random();
 
             List<string> nameMale = new List<string>()
             {
                 "Антон", "Виктор", "Андрей",
-                "Михаил","Борис", "Роман",
-                "Петр"
+                "Михаил","Борис", "Роман","Петр"
             };
 
             List<string> secondnameMale = new List<string>()
             {
                 "Стенов", "Любимов", "Стулов",
-                "Попов","Андреев", "Иванов", "Пушкин"
+                "Попов","Андреев", "Иванов", "Пушкин", "Подкорытов"
+            };
+
+            person.Gender = (Gender)randomMalePerson.Next(2);
+
+            var randomPersonNameMale = randomMalePerson.Next(0, nameMale.Count);
+            var randomPersonSecondnameMale = randomMalePerson.Next(0, secondnameMale.Count);
+            person.Name = nameMale[randomPersonNameMale];
+            person.SecondName = secondnameMale[randomPersonSecondnameMale];
+
+            person.Age = randomMalePerson.Next(person.MinAge, person.MaxAge);
+        }
+        /// <summary>
+        /// Метод для генерации случайной женщины
+        /// </summary>
+        /// <returns>Возвращает женщину, которая сгенерирована случайным образом</returns>
+        public static void FemaleRandomPerson(PersonBase person)
+        {
+            Random randomFemalePerson = new Random();
+
+            List<string> nameFemale = new List<string>()
+            {
+                "Валерия", "Алена", "Анна",
+                "Екатерина", "Алина", "Кира","Валентина"
             };
 
             List<string> secondnameFemale = new List<string>()
@@ -40,54 +57,29 @@ namespace LibraryPerson
                 "Мешанкина", "Сечина", "Ручкина", "Доскова"
             };
 
-            Random random = new Random();
-            randomPerson.Gender = (Gender)random.Next(2);
+            person.Gender = (Gender)randomFemalePerson.Next(2);
 
-            switch (randomPerson.Gender)
-            {
-                case Gender.Female:
-                    var randomPersonNameFemale = random.Next(0, nameFemale.Count);
-                    var randomPersonSecondnameFemale = random.Next(0, secondnameFemale.Count);
-                    randomPerson.Name = nameFemale[randomPersonNameFemale];
-                    randomPerson.SecondName = secondnameFemale[randomPersonSecondnameFemale];
-                    break;
+            var randomPersonNameFemale = randomFemalePerson.Next(0, nameFemale.Count);
+            var randomPersonSecondnameFemale = randomFemalePerson.Next(0, secondnameFemale.Count);
+            person.Name = nameFemale[randomPersonNameFemale];
+            person.SecondName = secondnameFemale[randomPersonSecondnameFemale];
 
-                case Gender.Male:
-                    var randomPersonNameMale = random.Next(0, nameMale.Count);
-                    var randomPersonSecondnameMale = random.Next(0, secondnameMale.Count);
-                    randomPerson.Name = nameMale[randomPersonNameMale];
-                    randomPerson.SecondName = secondnameMale[randomPersonSecondnameMale];
-                    break;
-
-                default:
-                    break;
-            }
-            randomPerson.Age = random.Next(person.MinAge, person.MaxAge);
+            person.Age = randomFemalePerson.Next(person.MinAge, person.MaxAge);
         }
 
-
         /// <summary>
-        /// Возвращает случайного взрослого с случайными параметрами.
+        /// Метод для генерации супружеской пары 
         /// </summary>
-        /// <param name="adult">Взрослый, для которого устанавливаются параметры.</param>
-        public static void SetRandomAdult(Adult adult)
+        /// <param name="secondName"></param>
+        /// <returns></returns>
+        public static (PersonBase husband, PersonBase wife) GetRandoMarriedCouple (string secondName)
         {
-            Random random = new Random();
-            string[] jobs = new string[];
+            var husband = MaleRandomPerson();
+            husband.SecondName = secondName;
 
-            List<string> job = new List<string>()
-            {
-                "ABC", "Пятерочка", "Красное-Белое", "Чиним двери", "ИА СО ЕЭС ОСВР",
-                "РОССЕТИ", "Скат", "S7", "Aston"
-            };
-
-            adult.Age = random.Next(adult.MinAge, adult.MaxAge);
-            if (random.Next(0, 2) == 0)
-            {
-                adult.Job = job[random.Next(0, job.Count)];
-            }
-            adult.PassportSeries = random.Next(1111, 9999);
-            adult.PassportNumber = random.Next(111111, 999999);
+            var husband = FemaleRandomPerson();
+            husband.SecondName = secondName;
+            return (husband, wife);
         }
     }
 }
