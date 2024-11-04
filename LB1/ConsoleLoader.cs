d7ab0e7ec4ecbd89d3398d2572ba3e6878589c09
eak94 <bykonyasha@main.ru>
2024-11-04 19:05:12 +0700

    feature/ доработка class ConsoleLoader

--- LB1/ConsoleLoader.cs
+++ LB1/ConsoleLoader.cs
@@ -14,9 +14,27 @@ namespace LB3
             {
                 Console.ForegroundColor = ConsoleColor.White;
 
-                Console.WriteLine($"Меню\n " +
-                    $"1 - Запустить расчет" +
-                          $"\nНажмите 2 - если хотите выйти из программы");
+                Console.WriteLine($"МЕНЮ\n " +
+                    $"1 - Запустить расчет\n" +
+                    $"Нажмите 2 - Выход из программы");
+
+                var number = Console.ReadLine();
+
+                switch (number)
+                {
+                    case "1":
+                    {
+                        Console.ForegroundColor = ConsoleColor.Blue;
+                        break;
+                    }
+                    case "2":
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Введите 1 или 2");
+                        break;
+
+                    }
+                }
 
             }
         }
