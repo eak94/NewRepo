ca009fd0bb644add11433249afcaa7c1dd30802e
eak94 <bykonyasha@main.ru>
2024-11-24 20:33:31 +0700

    fix/ LB4 SwimmingStyle

---
+++ Model/SwimmingStyle.cs
@@ -0,0 +1,33 @@
+namespace Model
+{
+    /// <summary>
+    /// Стиль плавания 
+    /// </summary>
+    public enum SwimmingStyle
+    {
+        /// <summary>
+        /// Брасс
+        /// </summary>
+        Brass = 1,
+
+        /// <summary>
+        /// Кроль-умеренный
+        /// </summary>
+        CrawlModerate = 2,
+
+        /// <summary>
+        /// Кроль-быстрый
+        /// </summary>
+        CrawlFast = 3,
+
+        /// <summary>
+        /// На спине
+        /// </summary>
+        Backstroke = 4,
+
+        /// <summary>
+        /// Дельфин
+        /// </summary>
+        Dolphin = 5
+    }
+}
